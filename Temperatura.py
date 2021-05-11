26499e05ffced38ba71e5eebd70567ec57f1e838
ThalesL <83838129+ThalesL@users.noreply.github.com>
2021-05-11 17:00:20 -0300

    Create Temperatura.py

---
+++ Temperatura.py
@@ -0,0 +1,8 @@
+temperatura = 32
+
+if temperatura <= 20:
+  print('Está muito frio, vou ficar em casa')
+elif temperatura > 20 and temperatura <= 32:
+  print('Está um clima bom para caminhar com meus amigos')
+else:
+  print('Está um clima bom para queimar calorias')
